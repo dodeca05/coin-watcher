7b3a66cafdb34c0c8be94aaf6e5658dc4c8292db
jaehyekim <jaehyekim@ebaykorea.com>
2024-03-15 00:29:35 +0900

    init commit

---
+++ src/main/java/com/dodeca/coinwatcher/CoinwatcherApplication.java
@@ -0,0 +1,13 @@
+package com.dodeca.coinwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinwatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinwatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dodeca/coinwatcher/CoinwatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodeca.coinwatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinwatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
